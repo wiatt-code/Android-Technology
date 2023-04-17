b096659cc41f1131c01dc8415de2e11f4de258a2
wiatt <1554980787@qq.com>
2023-04-17 16:31:44 +0800

    msg：更新Application的继承关系。 Application -> EngineApplication -> BaseApplication -> 各视图层模块的Application

---
+++ common/src/main/java/com/wiatt/common/CommonApplication.kt
@@ -0,0 +1,18 @@
+package com.wiatt.common
+
+import android.app.Application
+
+open class CommonApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        mApplication = this
+        LogUtil.init()
+
+
+    }
+
+    companion object {
+        var mApplication: CommonApplication? = null
+    }
+}
